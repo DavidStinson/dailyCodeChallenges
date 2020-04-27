6c802d6d9ba5aacf7cff97816d93134a055061eb
David Stinson <david.stinson.5@gmail.com>
2020-04-27 10:57:34 -0500

    Do waterBarGraph challenge

--- waterBarGraph/js/main.js
+++ waterBarGraph/js/main.js
@@ -12,6 +12,8 @@
 Write an algorithm which accepts an array of integers. Each integer represents the height of a bar in a bar graph. Your algorithm should calculate how much "water" could be stored in the bar graph. (Quick reference I googled, seems like this problem: https://www.geeksforgeeks.org/trapping-rain-water/)
 */
 
+// Alan did this!
+
 function findWater(arr) {
   let total = 0;
   let highest = 0;
